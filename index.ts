a73b689ecf6f51ca5c84bb6e5781ac6add249414
dejiborewa <ayodejiakinborewa@gmail.com>
2022-10-30 01:11:54 +0100

    fix: bio update

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const person = {
   slackUsername: "dejiborewa",
   backend: true,
   age: 26,
-  bio: "I am a Software Engineer with interest in applications that are performant, reliable and scalable",
+  bio: "I'm Ayodeji, a software engineer with interest in building applications that are performant, reliable and scalable.",
 };
 
 app.get("/", (req: Request, res: Response) => {
